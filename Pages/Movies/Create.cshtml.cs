68c10670135a42c42c25f63864ef03d7534ce6b4
Faizan Jamil <faizan34@gmail.com>
2019-06-16 18:18:29 -0400

    added necessary project files

---
+++ Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazerPagesMovie.Models;
+using RazorPagesMovie.Models;
+
+namespace RazerPagesMovie.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazerPagesMovie.Models.RazerPagesMovieContext _context;
+
+        public CreateModel(RazerPagesMovie.Models.RazerPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # learning-asp-net-core
+
 Contains all material related to learning ASP.NET Core as well as any other .NET related technologies
