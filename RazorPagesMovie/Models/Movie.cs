0c1892637f9df249b470f70a694e5452f6f71d87
faizjamil <faizan34@gmail.com>
2019-06-17 13:56:51 -0400

    added validation to some fields

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -7,14 +7,29 @@ namespace RazorPagesMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]  //validation
+        [Required]  //validation
         public string Title { get; set; }
+
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
-        public string Genre { get; set; }
 
+        [Range(1,100)]  //validation
+        [DataType(DataType.Currency)]  //validation
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]  //validation
+        [StringLength(30)]  //validation
+        [Required] //validation
+        public string Genre { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]  //validation
+        [StringLength(5)]  //validation
+        [Required]
         public string Rating { get; set; }
     }
 }
